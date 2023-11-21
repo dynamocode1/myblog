279f0c4496cae8ef683fd94cbbfca7bbcc2b115a
dynamocode1 <dunamiswinner539@gmail.com>
2023-11-21 19:59:04 +0100

    hosts

--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+cx%z54!!mm1^ak51c+n&9-(xna269t)kue$wl=0=d$(+nvrhe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
